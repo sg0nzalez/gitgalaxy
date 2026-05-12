bcaf175ba8d3b2ec45e13fa90d829d71e90ab45b
squid-protocol <joe.m.esquibel@gmail.com>
2026-05-12 08:30:15 -0400

    fix: remove undefined collections prefix from defaultdict

--- gitgalaxy/recorders/llm_recorder.py
+++ gitgalaxy/recorders/llm_recorder.py
@@ -88,8 +88,8 @@ class LLMRecorder:
 
 
         from collections import defaultdict
-        inbound_set_map = collections.defaultdict(set)
-        outbound_set_map = collections.defaultdict(set)
+        inbound_set_map = defaultdict(set)
+        outbound_set_map = defaultdict(set)
         
         for s in parsed_files:
             curr = s.get("path", "")
